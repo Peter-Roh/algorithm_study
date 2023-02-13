25e2755d7aab3bc0fd06a128478783ef2df3bcf0
Peter-Roh <minchul.roh.peter@gmail.com>
2023-02-13 20:37:32 +0900

    postfix calculator using stack

---
+++ study/Stack/Calculator.cpp
@@ -0,0 +1,48 @@
+#include <cassert>
+#include <iostream>
+#include "Stack.h"
+
+int main(void) {
+    // 후위표기법 stack 이용 계산 테스트
+    char expression[] = "245+*33/-"; // 17
+
+    Stack stack = Stack();
+
+    for(int i = 0; expression[i] != '\0'; ++i) {
+        char token = expression[i];
+        int res = 0;
+
+        if(token >= '0' && token <= '9') {
+            stack.Push(token - '0');
+        } else {
+            int num1 = stack.Pop();
+            int num2 = stack.Pop();
+
+            switch (token) {
+            case '+':
+                res = num2 + num1;
+                break;
+            case '-':
+                res = num2 - num1;
+                break;
+            case '*':
+                res = num2 * num1;
+                break;
+            case '/':
+                res = num2 / num1;
+                break;
+            default:
+                assert(0);
+                break;
+            }
+
+            stack.Push(res);
+        }
+    }
+
+    int ans = stack.Pop();
+
+    std::cout << "Answer: " << ans << std::endl;
+
+    return 0;
+}
